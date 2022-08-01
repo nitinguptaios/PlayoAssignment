ed77658f002d65bb19598334a87bab50eca963ef
Lekha-iphtech <nitin.gupta@iphtechnologies.com>
2022-08-01 18:48:41 +0530

    UI Created

--- PlayoAssignmentiOS/PlayoAssignmentiOS/PlayoProject/Controller/HomeVC.swift
+++ PlayoAssignmentiOS/PlayoAssignmentiOS/PlayoProject/Controller/HomeVC.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    @IBOutlet weak var newsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
     }
 
+}
 
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
 }
 
